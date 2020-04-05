0e82d5e6e44f145e629c5aef8f67b46a01740ebd
jeferwang <jeferwang@qq.com>
2020-04-06 00:21:02 +0800

    generate xml

---
+++ src/demo_xml/demo_xml.cpp
@@ -0,0 +1,42 @@
+//
+// Created by jefer on 2020/4/5.
+//
+
+#include "demo_xml.h"
+
+void createXml() {
+    rapidxml::xml_document<> doc;
+    rapidxml::xml_node<> *rot;
+    rot = doc.allocate_node(rapidxml::node_pi, doc.allocate_string(R"(xml version="1.0" encoding="utf-8")"));
+    doc.append_node(rot);
+
+    rapidxml::xml_node<> *node = doc.allocate_node(rapidxml::node_element, "config", "information");
+    doc.append_node(node);
+
+    rapidxml::xml_node<> *color = doc.allocate_node(rapidxml::node_element, "color", nullptr);
+    node->append_node(color);
+    color->append_node(doc.allocate_node(rapidxml::node_element, "red", "0.1"));
+    color->append_node(doc.allocate_node(rapidxml::node_element, "green", "0.2"));
+    color->append_node(doc.allocate_node(rapidxml::node_element, "blue", "0.3"));
+
+    rapidxml::xml_node<> *size = doc.allocate_node(rapidxml::node_element, "size", nullptr);
+    size->append_node(doc.allocate_node(rapidxml::node_element, "x", "1920"));
+    size->append_node(doc.allocate_node(rapidxml::node_element, "y", "1080"));
+    node->append_node(size);
+
+    rapidxml::xml_node<> *mode = doc.allocate_node(rapidxml::node_element, "mode", "screen mode");
+    mode->append_attribute(doc.allocate_attribute("fullscreen", "false"));
+    node->append_node(mode);
+
+    std::string text;
+    rapidxml::print(std::back_inserter(text), doc, 0);
+
+    printf("%s", text.c_str());
+
+}
+
+
+int main() {
+    createXml();
+    return 0;
+}
---
+++ src/demo_xml/demo_xml.h
@@ -0,0 +1,11 @@
+//
+// Created by jefer on 2020/4/5.
+//
+
+#ifndef DEMO_CPP_JSON_XML_PARSE_DEMO_XML_H
+#define DEMO_CPP_JSON_XML_PARSE_DEMO_XML_H
+
+#include "rapidxml/rapidxml.hpp"
+#include "rapidxml/rapidxml_print.hpp"
+
+#endif //DEMO_CPP_JSON_XML_PARSE_DEMO_XML_H
