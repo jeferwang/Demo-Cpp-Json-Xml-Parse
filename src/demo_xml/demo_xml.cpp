6bcbbc1669dabbafb9d19f206b28bfcac26aec9a
jeferwang <jeferwang@qq.com>
2020-04-06 00:39:36 +0800

    parse xml

--- src/demo_xml/demo_xml.cpp
+++ src/demo_xml/demo_xml.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "demo_xml.h"
-
+using namespace std;
 void createXml() {
     rapidxml::xml_document<> doc;
     rapidxml::xml_node<> *rot;
@@ -32,11 +32,32 @@ void createXml() {
     rapidxml::print(std::back_inserter(text), doc, 0);
 
     printf("%s", text.c_str());
+}
 
+void readXml() {
+    rapidxml::xml_document<> doc;
+    char szBuf[] = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n"
+                   "<config>\n"
+                   "        <color>\n"
+                   "                <red>0.1</red>\n"
+                   "                <green>0.2</green>\n"
+                   "                <blue>0.3</blue>\n"
+                   "        </color>\n"
+                   "        <size>\n"
+                   "                <x>1920</x>\n"
+                   "                <y>1080</y>\n"
+                   "        </size>\n"
+                   "        <mode fullscreen=\"false\">screen mode</mode>\n"
+                   "</config>";
+    doc.parse<0>(szBuf);
+//    std::cout << doc.name() << std::endl;
+    rapidxml::xml_node<> *root = doc.first_node();
+    cout<<root->name()<<endl;
 }
 
 
 int main() {
-    createXml();
+//    createXml();
+    readXml();
     return 0;
 }
--- src/demo_xml/demo_xml.h
+++ src/demo_xml/demo_xml.h
@@ -5,7 +5,9 @@
 #ifndef DEMO_CPP_JSON_XML_PARSE_DEMO_XML_H
 #define DEMO_CPP_JSON_XML_PARSE_DEMO_XML_H
 
+#include <iostream>
 #include "rapidxml/rapidxml.hpp"
 #include "rapidxml/rapidxml_print.hpp"
+#include "rapidxml/rapidxml_utils.hpp"
 
 #endif //DEMO_CPP_JSON_XML_PARSE_DEMO_XML_H
